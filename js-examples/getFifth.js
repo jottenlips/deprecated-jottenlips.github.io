47e52c211f44d4dc5809a22374fc98b9866a5250
johno <johno@aloompa.com>
2019-10-28 09:11:59 -0500

    circle of fifths

---
+++ js-examples/getFifth.js
@@ -0,0 +1,24 @@
+const getFifth = (note) => {
+    const lowerCaseNote = note.toLowerCase()
+    const fifth = {
+        c: 'd',
+        g: 'd',
+        d: 'a',
+        a: 'e',
+        e: 'b',
+        b: 'f#',
+        'f#': 'c#',
+        gb: 'db',
+        'c#': 'ab',
+        db: 'ab',
+        ab: 'eb',
+        eb: 'bb',
+        bb:'f',
+        f: 'c'
+    }[lowerCaseNote]
+    const formattedNote = `${fifth.charAt(0).toUpperCase()}${fifth.slice(1)}`
+    return formattedNote
+}
+
+const fifthOfGb = getFifth('gb')
+console.log(`The fifth of Gb is ${fifthOfGb}`)
